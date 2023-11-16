028915a42bf1efa25227d8dc4e0889cd60a4ee92
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-15 19:06:15 -0700

    Initial commit
    Add merge sort

---
+++ app.js
@@ -0,0 +1,24 @@
+function mergeSort (arr) {
+    const half = Math.ceil(arr.length / 2);
+    let left;
+    let right;
+    let foo = [];
+    if (arr.length === 1) {
+        return arr;
+    } else {
+        left = mergeSort(arr.slice(0, half));
+        right = mergeSort(arr.slice(half));
+    }
+    while (left.length > 0 || right.length > 0) {
+        if (left[0] < right[0] || right.length === 0) {
+            foo.push(left[0]);
+            left.shift();
+        } else if (right[0] < left[0] || left.length === 0) {
+            foo.push(right[0]);
+            right.shift();
+        }
+    }
+    return foo;
+}
+
+//console.log(mergeSort([3, 2, 7, 4, 8, 5, 1, 6]));
