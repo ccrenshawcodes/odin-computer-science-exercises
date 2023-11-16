ed20e2fae83c09fd5371db67169ceaca2d979450
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-16 16:24:55 -0700

    Rearrange variable declarations to minimize steps

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 function mergeSort (arr) {
-    const half = Math.ceil(arr.length / 2);
-    let left;
-    let right;
-    let sortedArr = [];
     if (arr.length === 1) {
         return arr;
     } else {
-        left = mergeSort(arr.slice(0, half));
-        right = mergeSort(arr.slice(half));
-    }
-    while (left.length > 0 || right.length > 0) {
-        if (left[0] < right[0] || right.length === 0) {
-            sortedArr.push(left[0]);
-            left.shift();
-        } else if (right[0] < left[0] || left.length === 0) {
-            sortedArr.push(right[0]);
-            right.shift();
+        const half = Math.ceil(arr.length / 2);
+        const left = mergeSort(arr.slice(0, half));
+        const right = mergeSort(arr.slice(half));
+
+        const sortedArr = [];
+        while (left.length > 0 || right.length > 0) {
+            if (left[0] < right[0] || right.length === 0) {
+                sortedArr.push(left[0]);
+                left.shift();
+            } else if (right[0] < left[0] || left.length === 0) {
+                sortedArr.push(right[0]);
+                right.shift();
+            }
         }
+        return sortedArr;
     }
-    return sortedArr;
 }
 
-//console.log(mergeSort([3, 2, 7, 4, 8, 5, 1, 6]));
+console.log(mergeSort([5, 7, 3, 2, 1, 4]));
